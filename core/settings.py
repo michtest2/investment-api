8a3ce535494946d3a2018a49d34cb4e4c54d53e5
michtest2 <mich@x.com>
2025-02-10 20:22:19 +0100

    import dj_database_url

--- core/settings.py
+++ core/settings.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from datetime import timedelta
 import os
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
