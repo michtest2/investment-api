9e3f8632c5bd2e610cc933e0077489ae0b16c652
michtest2 <mich@x.com>
2025-02-10 19:51:46 +0100

    added static

--- core/urls.py
+++ core/urls.py
@@ -18,6 +18,9 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 schema_view = get_schema_view(
     openapi.Info(
         title="Your API Title",
@@ -67,3 +70,5 @@ urlpatterns = [
     # Raw JSON schema
     path("swagger.json", schema_view.without_ui(cache_timeout=0), name="schema-json"),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
