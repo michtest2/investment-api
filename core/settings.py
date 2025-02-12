b3d5c5c385b8e6bef98a92a3646a1665d762c281
michtest2 <mich@x.com>
2025-02-12 02:11:28 +0100

    changed redis from localhost to prod

--- core/settings.py
+++ core/settings.py
@@ -163,10 +163,10 @@ DEFAULT_FROM_EMAIL = "damy@trial-3yxj6lj8385gdo2r.mlsender.net"
 SITE_ID = 1
 
 # celery
-CELERY_BROKER_URL = "redis://localhost:6379/0"
-CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
-# CELERY_BROKER_URL = "redis://host.docker.internal:6379/0"
-# CELERY_RESULT_BACKEND = "redis://host.docker.internal:6379/0"
+# CELERY_BROKER_URL = "redis://localhost:6379/0"
+# CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
+CELERY_BROKER_URL = "redis://host.docker.internal:6379/0"
+CELERY_RESULT_BACKEND = "redis://host.docker.internal:6379/0"
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
