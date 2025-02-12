23fac2d41e3837cf3abeee8e45ff8ecb79403e8f
michtest2 <mich@x.com>
2025-02-12 02:32:16 +0100

    removed previoys rdis

--- core/settings.py
+++ core/settings.py
@@ -166,9 +166,6 @@ SITE_ID = 1
 # CELERY_BROKER_URL = "redis://localhost:6379/0"
 # CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
 
-# docker redis
-# CELERY_BROKER_URL = "redis://host.docker.internal:6379/0"
-# CELERY_RESULT_BACKEND = "redis://host.docker.internal:6379/0"
 
 # render redis
 
