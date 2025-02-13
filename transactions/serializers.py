9c34da10a73714bbb52fff4c2a212faedee4fb41
michtest2 <michellerogers1901@yahoo.com>
2025-02-14 00:06:45 +0100

    Update serializers.py
    Created new deposit serializer

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -69,3 +69,35 @@ class DepositSerializer(serializers.ModelSerializer):
         obj.plan = plan
         obj.save()
         return InvestmentPlanSerializer(plan).data["name"]
+
+
+class DepositViewSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
+    # user = serializers.StringRelatedField()  # Display the user's string representation
+    # transaction = TransactionSerializer()  # Display the transaction's data
+    #plan = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Deposit
+        fields = "__all__"  # Includes all fields from the model
+        read_only_fields = (
+            "id",
+            "created_at",
+            "updated_at",
+        )  # Prevent updates on these fields
+
+    def get_user(self, obj):
+        # Return the user's string representation
+        return str(obj.user)
+
+    def create(self, validated_data):
+        # Set the user from the request context
+        validated_data["user"] = self.context["user"]
+        return super().create(validated_data)
+
+    #def get_plan(self, obj):
+       # plan = InvestmentPlan.objects.get(name=self.context["plan_name"])
+       # obj.plan = plan
+        #obj.save()
+       # return InvestmentPlanSerializer(plan).data["name"]
+        
