0aca3778a5ce2fc4dd9f12c167a604728fd61326
michtest2 <mich@x.com>
2025-02-20 15:33:57 +0100

    done

--- core/settings.py
+++ core/settings.py
@@ -166,7 +166,7 @@ SIMPLE_JWT = {
     "AUTH_COOKIE_SECURE": True,  # Only send cookies over HTTPS
     "AUTH_COOKIE_HTTP_ONLY": True,  # Protect cookies from JavaScript access
     "AUTH_COOKIE_PATH": "/",
-    "AUTH_COOKIE_SAMESITE": "None",
+    "AUTH_COOKIE_SAMESITE": "none",
     "SIGNING_KEY": SECRET_KEY,
 }
 AUTH_USER_MODEL = "accounts.User"
