b7a80f47423ca12c0c58fb0481c8890760632d55
michtest2 <mich@x.com>
2025-02-20 15:07:11 +0100

    logged_out

--- dashboard/serializers.py
+++ dashboard/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 from .models import Dashboard
+from referrals.models import Referral
 
 
 class DashboardSerializer(serializers.ModelSerializer):
     registration_date = serializers.DateField(format="%b-%d-%Y")
     last_access = serializers.DateTimeField(format="%b-%d-%Y %I:%M:%S %p")
+    referrals_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Dashboard
@@ -17,4 +19,10 @@ class DashboardSerializer(serializers.ModelSerializer):
             "pending_withdrawal",
             "total_withdrawal",
             "active_deposit",
+            "referrals_count",
         ]
+
+    # get count of referrals
+    def get_referrals_count(self, obj):
+        # get user from obj, get user referrals and count them
+        return Referral.objects.filter(referrer=obj.user).count()
